daa1fb2b7f7d6592b5c252b74846c18f808cda82
Marcos Henrique <59567130+RiderBR@users.noreply.github.com>
2020-07-04 00:24:41 -0300

    EventEmitter com node no javascript

---
+++ JavaScript ES6/Funcoes_avançadas/EventEmitter.js
@@ -0,0 +1,16 @@
+//EvenEmitter - Disponivel apenas em node
+const EventEmitter = require('events');
+class Users extends EventEmitter {
+    userLogged(data) {
+        setTimeout(() => {
+            this.emit('User logged', data);
+        }, 2000);
+    }
+}
+const users = new Users();
+users.on('User logged', data => {
+    console.log(data);
+});
+
+users.userLogged({user: 'Marcos Miranda'});
+users.userLogged({user: 'Junior Andrade'});
