959e96eead7ce32536f5e32fc90f019915512411
Marcos Henrique <59567130+RiderBR@users.noreply.github.com>
2020-07-05 16:55:42 -0300

    TDD e BDD
    Exemplo em código para teste em mocha.

---
+++ Mocha/test/math.specs.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const Math = require('../src/math.js');
+
+describe('Math class', function() {
+    it('Sum two numbers', function(done) {
+        const math = new Math();
+        this.timeout(3000);
+        mathe.sum(5, 5, (value) => {
+            assert.equal(value, 10);
+            done();
+        });
+    });
+    it('Multiply two numbers');
+});
