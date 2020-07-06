d96593c43fc017141c40e8674d07c707fc043fb9
Marcos Henrique <59567130+RiderBR@users.noreply.github.com>
2020-07-06 15:10:12 -0300

    ReactJS
    Aula pratica sobre o básico de ReactJS e algumas funções da biblioteca.

---
+++ React/Pratica/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Titulo from './componentes/Titulo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Live Digital Inovation</h1>
+      <Titulo>
+        Meu primeiro filho.
+      </Titulo>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Pratica/my-app/src/componentes/Titulo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import "./titulo.css";
+
+function Titulo({ children, cor }) {
+    return (<h1 className={cor}>{children} {cor}</h1>);
+}
+
+Titulo.propTypes = {
+    children: PropTypes.node.isRequired,
+    cor: PropTypes.oneOf(["vermelho", "azul", "verde"]),
+};
+
+Titulo.defaultProps = {
+    cor: "vermelho",
+}
+
+export default Titulo;
